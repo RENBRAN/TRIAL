ba2dd73e993b64bf0b08f707edc819ae496760ad
RENBRAN <139387428+RENBRAN@users.noreply.github.com>
2024-03-25 02:16:49 +0400

    advance payment and filter

---
+++ eg_so_today_yesterday_filter-17.0.17.0/eg_so_today_yesterday_filter/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Today Yesterday filter in SO',
+    'version': '17.0',
+    'category': 'Sale',
+    'summery': 'This app will display Current dates sale order and the previous days sale order.',
+    'author': 'INKERP',
+    'website': "https://www.INKERP.com",
+    'depends': ['sale_management'],
+    
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    
+    'images': ['static/description/banner.png'],
+    'license': "OPL-1",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ eg_so_today_yesterday_filter-17.0.17.0/eg_so_today_yesterday_filter/models/sale_order.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
